b452cd299df382142312492b0d4bb5ca11cbd4df
mohit-52 <105234980+mohit-52@users.noreply.github.com>
2022-07-19 12:53:25 +0530

    Create main.kt

---
+++ main.kt
@@ -0,0 +1,15 @@
+import java.util.*
+
+fun main() {
+   
+   val string = "“I am a Kotlin programmer now"
+                  
+   
+   val indexArray:Array<Int> = arrayOf(0,2,5,8,11,16,19,22,26)
+   
+   println(Arrays.toString(indexArray))
+   
+   var sum = indexArray[0]+indexArray[1]+indexArray[2]+indexArray[3]+indexArray[4]+indexArray[5]+indexArray[6]+indexArray[7]+indexArray[8]
+   
+   println(sum)
+}
